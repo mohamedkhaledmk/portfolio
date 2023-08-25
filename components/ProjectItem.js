0baf5e2dac94ac449647302fa920e408722f5b6c
mohamedkhaledmk <66388583+mohamedkhaledmk@users.noreply.github.com>
2023-08-25 21:08:45 +0300

    fixing links

--- components/ProjectItem.js
+++ components/ProjectItem.js
@@ -15,7 +15,7 @@ const ProjectItem = ({ title, backgroundImg, tech, projectUrl }) => {
           {title}
         </h3>
         <p className="pb-4 pt-2 text-white text-center">{tech}</p>
-        <Link href={projectUrl}>
+        <Link href={projectUrl} target="_blank">
           <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">
             More Info
           </p>
